3305352112f01298f1bab7fcb24c3fabd3cf81b8
sotniko-aleksand <sotniko-aleksand@yandex.ru>
2024-12-25 14:56:36 +0500

    Add files via upload

---
+++ mobule_1_3.py
@@ -0,0 +1,10 @@
+name = 'Aleksandr'
+print(name, type(name))
+age = 50
+print(age, type(age))
+print(age, type(name))
+newage = '50'
+print(newage, type(newage))
+is_student = True
+print(is_student, type(is_student))
+print(name, + age)
