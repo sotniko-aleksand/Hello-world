22bf0eb887db5939fee913d027f0f66fe44716fc
sotniko-aleksand <sotniko-aleksand@yandex.ru>
2024-12-27 00:47:43 +0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+the_number_of_completed_DZ = 12
+the_number_of_hours_spent = 1.5
+Course_name = 'Python'
+time_per_task = (the_number_of_hours_spent / the_number_of_completed_DZ)
+print(Course_name,the_number_of_completed_DZ,the_number_of_hours_spent,time_per_task)
