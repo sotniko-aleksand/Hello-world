0df79abdce8681d21e92535b15175e856dd283dd
sotniko-aleksand <sotniko-aleksand@yandex.ru>
2025-01-08 18:21:13 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input("Введите кличку питомца: " )
+print("красивое прозвеще," ,my_string)
+print('самое красивое прозвище для кота' .upper())
+print('Самое красивое прозвище для кота' .lower())
+print('Самое красивое прозвище для кота' .replace(' ',''))
+my_string = 'lyntik'
+print(my_string [0])
+print(my_string[-1])
